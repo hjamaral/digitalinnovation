f73a2f7311add48f058b9031c7154221ff889cd6
hjamaral <hjamaral@gmail.com>
2021-04-16 00:16:12 -0300

    Fundamentos de OO com Kotlin [parte 1]

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/Conta.kt
@@ -0,0 +1,17 @@
+package one.digitalinnovation.digionebank
+
+import com.sun.org.apache.xml.internal.utils.StringVector
+import java.math.BigDecimal
+
+class Conta {
+    val agencia: String,
+    val numero: String,
+    val saldo: BigDecimal,
+} {
+    fun deposito(valor:BigDecimal) {
+
+    }
+    fun saque(valor:BigDecimal) {
+
+    }
+}
---
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -0,0 +1,19 @@
+package src.main.kotlin.p.one.digitalinnovation.digionebank
+
+class Pessoa(
+    var nome: String = "Hernane"
+    var cpf: String = "123.456.789-01"
+
+    Constructor()
+    fun pessoaInfo() = "$nome e $cpf"
+)
+
+fun main() {
+    val eu = Pessoa()
+    eu.cpf = "123"
+
+    println(eu)
+    println(eu.nome)
+    println(eu.cpf)
+    println(eu.pessoaInfo())
+}
