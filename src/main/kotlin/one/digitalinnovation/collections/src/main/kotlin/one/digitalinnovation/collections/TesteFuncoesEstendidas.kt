7624318b5a60deb7728f268a0e633a9666daee09
hjamaral <hjamaral@gmail.com>
2021-05-23 17:12:00 -0300

    Modulo: Aplicando conceitos de Coleções, Arrays e Listas Aula: Praticando com Extensions Functions

---
+++ src/main/kotlin/one/digitalinnovation/collections/src/main/kotlin/one/digitalinnovation/collections/TesteFuncoesEstendidas.kt
@@ -0,0 +1,15 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val salarios = arrayOf(
+        "2000".toBigDecimal(),
+        "1500".toBigDecimal(),
+        "4000".toBigDecimal()
+    )
+
+    println("--------------------- somatoria")
+    println(salarios.somatoria())
+    println("--------------------- media")
+    println(salarios.media())
+
+}
